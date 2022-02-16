dabea77adeae9975692c850b92d9b047887d818e
Daniil Safiulin <73942815+DaniilSafiulin-NDX@users.noreply.github.com>
2022-02-16 15:18:30 +0700

    Add files via upload

---
+++ src/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="git-btn">
+    <my-button>
+      Оставьте ваш отзыв о моей работе
+    </my-button>
+    <my-select
+        v-model="selectedSort"
+        :options="sortOptions"
+      />
+  </div>
+</template>
+
+<script>
+import MyButton from '../components/UI/MyButton.vue'
+export default {
+  components: { MyButton },
+
+}
+</script>
+
+<style scoped>
+
+</style>
